a2f778b75f34aaacf1210b06b08f33228c8616c1
Akane0syam <100626383+Akane0syam@users.noreply.github.com>
2024-06-09 23:42:49 +0900

    フラッシュメッセージを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -50,6 +50,7 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
+            // データベースに保存
             em.persist(m);
             em.getTransaction().commit();
             request.getSession().setAttribute("flush", "登録が完了しました。");
